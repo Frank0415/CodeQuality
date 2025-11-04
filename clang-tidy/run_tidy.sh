04e3820e5a4036294d610fa4b3abadc85e878988
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-04 16:52:18 +0800

    fix: stricter tidy

--- clang-tidy/run_tidy.sh
+++ clang-tidy/run_tidy.sh
@@ -1,4 +1,4 @@
-./run_clang-tidy.py -checks="-*,clang-analyzer-*,bugprone-*,cppcoreguidelines-*,misc-*,modernize-*,-modernize-use-trailing-return-type,performance-*,portability-*,readability-*,google-*,-google-objc-function-naming" -warnings-as-errors='*' -extra-arg-before=-std=c++20 -extra-arg-before=-x -extra-arg-before=c++ -header-filter=.* > clang_tidy.txt
+./run_clang-tidy.py -p . -warnings-as-errors='*' -extra-arg-before=-std=c++20 -extra-arg-before=-x -extra-arg-before=c++ -header-filter=".*" > clang_tidy.txt
 
 # Remove everything before "Running clang-tidy" line
 sed -i '/^Running clang-tidy/,$!d' clang_tidy.txt
