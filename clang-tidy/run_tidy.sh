7396f0fc6edd26678514fd93020fbc4bc8856166
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-21 14:23:42 +0800

    fix: redo tidy sh

--- clang-tidy/run_tidy.sh
+++ clang-tidy/run_tidy.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+cmake -S . -B build -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+cp build/compile_commands.json compile_commands.json
+
 ./run_clang-tidy.py -p . -warnings-as-errors='*' -extra-arg-before=-std=c++20 -extra-arg-before=-x -extra-arg-before=c++ -header-filter=".*" > clang_tidy.txt
 
 # Remove everything before "Running clang-tidy" line
-sed -i '/^Running clang-tidy/,$!d' clang_tidy.txt
+# sed -i '/^Running clang-tidy/,$!d' clang_tidy.txt
 
 echo "Clang-tidy complete."
